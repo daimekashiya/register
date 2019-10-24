6acbad5f1cc6a8c3d4ab50e1e80a99c4415374b8
Akashah Ahamad <akashah@2enapps.my>
2019-10-24 16:39:47 +0800

    Update create2.blade.php

--- resources/views/membership/create2.blade.php
+++ resources/views/membership/create2.blade.php
@@ -7,20 +7,49 @@
     
     <!-- - - - - - - - - - - - - - Settings View Products - - - - - - - - - - - - - - - - -->
         <header class="settings-view-products">
-
             <h5 class="widget-title">{{__('Register')}}</h5>
-
         </header>
-    <!-- - - - - - - - - - - - - - End of Settings View Products - - - - - - - - - - - - - - - - -->
+    <!-- - - - - - - - - - - - - - End of Settings View Products - - - - - - - - - -- - - -->
 
     <!-- - - - - - - - - - - - - - Products Holder - - - - - - - - - - - - - - - - -->
-
     <div class="products-holder with-separators view-grid fx-cols-3" id="product-holder">
 
 
-    </div>
+        <form class="form-style3 with-icon" style="margin:3%" method="POST" action="{{ route('register') }}">
+            @csrf
+            <div class="form-column-full">
+                <div class="relative">
+                    <input type="text" name="email" value="{{ old('email') }}" aria-describedby="emailHelp"  required autocomplete="email" autofocus  placeholder="{{__('layouts.email_address')}}">
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+            </div>
 
-    <!-- - - - - - - - - - - - - - End of Products Holder - - - - - - - - - - - - - - - - -->
+
+            <div class="form-column-full">
+                <div class="relative">
+                    <input type="password" name="password" required autocomplete="current-password"  placeholder="{{__('layouts.password')}}">
+                </div>
+            </div>
+
+            <div class="form-group form-check" style="display:none;">
+                <input type="checkbox"  name="remember" {{ old('remember') ? 'checked' : null }}>
+                <label class="form-check-label" for="exampleCheck1">Stay Logged In</label>
+            </div>
+            <input type="hidden" name="form[method]" value="login">
+
+
+            <div class="form-column-full">
+                <button type="submit" class="btn btn-primary float-right">{{__('Login')}}</button>
+            </div>
+
+        </form>
+    
+    </div>
+    <!-- - - - - - - - - - - - - - End of Products Holder - - - - - - - - - - - - - -->
 
     </div>
 
